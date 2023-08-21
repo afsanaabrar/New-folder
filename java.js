5796a1ee7b412ce0604147462ef7f03bbb5d8048
afsanaabrar <afrojajebu@gmail.com>
2023-08-20 17:54:51 -0700

    first commit

---
+++ java.js
@@ -0,0 +1,22 @@
+
+
+
+
+// function firstCard(target){
+//    const innText=target.parentNode.childNodes[3].childNodes[7].childNodes[1].innerText
+//    const innTextParseFloat=parseFloat(innText)
+    // console.log(innTextParseFloat)
+    // show name another dive
+    // const nameShow=target.parentNode.childNodes[3].childNodes[5].innerText;
+    
+    // create an p tag and set list dive
+    // const itmName=document.getElementById('item-name');
+    // const pTag=document.createElement('p');
+    //  pTag.innerText=nameShow;
+    //  itmName.appendChild(pTag)
+
+    
+}
+
+
+
